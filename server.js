1d94656ca10cc1332a3622804b45edfccbec1a45
Yohannes <jhon.klaus2008@gmail.com>
2024-03-27 13:45:49 +0300

    setting up middlewares

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
 
 //database
 const connectDB = async () => {
@@ -15,6 +23,14 @@ const connectDB = async () => {
 
 //middlewares
 dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
 app.listen(process.env.PORT, () => {
   connectDB();
